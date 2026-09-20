8005ad2421d0ec3b434a01d378935b5a9ece54a8
Gs <josephleonvivarjeremias@gmail.com>
2026-09-19 22:05:56 -0500

    Lol

--- LimbExtenderPrueba.lua
+++ LimbExtenderPrueba.lua
@@ -3,7 +3,7 @@ local localPlayer = Players.LocalPlayer
 
 local DEFAULTS = {
 	TOGGLE = "L",
-	TARGET_LIMB = "HumanoidRootPart",
+	TARGET_LIMB = "UpperTorso",
 	LIMB_SIZE = 15,
 	LIMB_TRANSPARENCY = 0.9,
 	LIMB_CAN_COLLIDE = false,
@@ -12,7 +12,7 @@ local DEFAULTS = {
 	TEAM_CHECK = false,
 	FORCEFIELD_CHECK = false,
 	RESET_LIMB_ON_DEATH2 = false,
-	USE_HIGHLIGHT = true,
+	USE_HIGHLIGHT = false,
 	DEPTH_MODE = "AlwaysOnTop",
 	HIGHLIGHT_FILL_COLOR = Color3.fromRGB(0,0,0),
 	HIGHLIGHT_FILL_TRANSPARENCY = 0.8,
