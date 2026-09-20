2df2f10789e299420275ab43150a95a668f9c5b9
Gs <josephleonvivarjeremias@gmail.com>
2026-09-19 22:12:38 -0500

    lol

--- LimbExtenderPrueba.lua
+++ LimbExtenderPrueba.lua
@@ -3,7 +3,7 @@ local localPlayer = Players.LocalPlayer
 
 local DEFAULTS = {
 	TOGGLE = "L",
-	TARGET_LIMB = "UpperTorso",
+	TARGET_LIMB = "HumanoidRootPart",
 	LIMB_SIZE = 15,
 	LIMB_TRANSPARENCY = 0.9,
 	LIMB_CAN_COLLIDE = false,
